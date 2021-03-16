3b2624fc742bcd86fce9672bd23c5eee9e1ea2cb
Andrew Coyle <coyleandrewi@gmail.com>
2021-03-16 00:32:02 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,75 @@
+'''
+This method calulates the Manhattan distance between two points.
+The Manhattan distance is calculated by only taking the vertical and horizontal paths.
+This method takes in two tuple values and returns an integer.
+Each tuple represents a point on a grid. The return value is the Manhattan distance between those points
+'''
+def manhattanDistance(first_point, second_point):
+    # Calculate the horizontal distance: | x1 - x2 |
+    x_distance = abs(first_point[0] - second_point[0])
+
+    # Calculate the vertical distance: | y1 - y2 |
+    y_distance = abs(first_point[1] - second_point[1])
+
+    # Total distance is horizontal distance + vertical distance
+    manhattanDistance = x_distance + y_distance
+
+    return manhattanDistance
+
+'''
+This method validates user input to ensure the user is entering integer values for the coordinates.
+This method takes in user input and attempts to cast it to an integer. If the casting is unsuccessful,
+then the method will return false. If casting is successful, this method returns true.
+'''
+def validInput(input):
+    # Try to convert the input to an int. If this fails, then we return false.
+    try:
+        int(input)
+        return True
+    except ValueError:
+        return False
+
+'''
+This is the entry point to our program. The main method will continue to ask for user input until
+the user is finished calculating distances.
+'''
+if __name__ == "__main__":
+    print("Please enter two points to calculate the Manhattan distance. Values entered must be integers.")
+
+    while True:
+        # Prompt user to enter the x-coordinate for the first point; validate input
+        first_x_coord = input("Please enter x-coordinate of first point: ")
+        while not validInput(first_x_coord):
+            print("ERROR: Input must be an integer")
+            first_x_coord = input("Please enter x-coordinate of first point: ")
+
+        # Prompt user to enter the y-coordinate for the first point; validate input
+        first_y_coord = input("Please enter y-coordinate of first point: ")
+        while not validInput(first_y_coord):
+            print("ERROR: Input must be an integer")
+            first_y_coord = input("Please enter y-coordinate of first point: ")
+
+        # Prompt user to enter the x-coordinate for the second point; validate input
+        second_x_coord = input("Please enter x-coordinate of second point: ")
+        while not validInput(second_x_coord):
+            print("ERROR: Input must be an integer")
+            second_x_coord = input("Please enter x-coordinate of second point: ")
+
+        # Prompt user to enter the y-coordinate for the second point; validate input
+        second_y_coord = input("Please enter y-coordinate of second point: ")
+        while not validInput(second_y_coord):
+            print("ERROR: Input must be an integer")
+            second_y_coord = input("Please enter y-coordinate of second point: ")
+
+        # Create points
+        first_point = (int(first_x_coord), int(first_y_coord))
+        second_point = (int(second_x_coord), int(second_y_coord))
+
+        # Calculate the manhattan distance and print the value
+        result = manhattanDistance(first_point, second_point)
+        print("The Manhattan distance is: " + str(result))
+        user_input = input("Would you like to calculate another distance? (y/n): ")
+
+        # Jump out of loop if user wishes to exit
+        if user_input == "n":
+            break
